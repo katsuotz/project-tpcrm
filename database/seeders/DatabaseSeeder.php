bc71900b88d86d31119e6e33c685cc97787c2347
Irfan Fakhri <m.irfan.fakhri66@gmail.com>
2023-07-17 17:34:22 +0700

    fix user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,15 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-//         \App\Models\User::factory(10)->create();
-
-//         \App\Models\User::factory()->create([
-//             'username' => 'admin',
-//         ]);
-
-        \App\Models\User::create([
+        \App\Models\User::factory()->create([
             'username' => 'admin',
             'password' => bcrypt('admin'),
         ]);
+
+         \App\Models\User::factory(10)->create();
     }
 }
